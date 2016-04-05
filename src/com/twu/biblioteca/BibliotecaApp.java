32dee1856fbef5664684220a55c3512c84afbb96
Albert Salim <albertlimca@gmail.com>
2016-04-05 16:33:45 +0800

    app will run until user types 'Quit'

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,8 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
   private String promptMessage = "Welcome to Biblioteca.";
+  private String input = "";
+  Helper helper = new Helper();
 
 
   public String getPromptMessage() {
@@ -11,5 +13,11 @@ public class BibliotecaApp {
 
   public void start() {
     System.out.println(promptMessage);
+    while (!input.equals("Quit")) {
+      promptMessage = "How can I help you?\nType 'Quit' to leave Biblioteca.";
+      input = helper.getUserInput(promptMessage);
+      System.out.println(input);
+    }
+    return;
   }
 }
