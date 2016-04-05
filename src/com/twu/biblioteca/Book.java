a0631fb4655944935103e8b3cd92052d9952fbb8
Albert Salim <albertlimca@gmail.com>
2016-04-05 16:38:59 +0800

    test for Book constructor

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by albert on 5/4/16.
+ */
+public class Book {
+}
---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,18 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by albert on 5/4/16.
+ */
+public class BookTest {
+  @Test
+  public void bookHasTitleAuthorYear() {
+    Book harryPotter = new Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 1997);
+    assertEquals(harryPotter.getTitle(), "Harry Potter and the Philosopher's Stone");
+    assertEquals(harryPotter.getAuthor(), "J. K. Rowling");
+    assertEquals(harryPotter.getYear(), 1997);
+  }
+}
