3b50d02f625f1bf3d659ef19fa291a7e726b06eb
Albert Salim <albertlimca@gmail.com>
2016-04-05 16:41:56 +0800

    create constructor for Book

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -4,4 +4,23 @@ package com.twu.biblioteca;
  * Created by albert on 5/4/16.
  */
 public class Book {
+  private String title;
+  private String author;
+  private int year;
+
+  Book(String bookTitle, String bookAuthor, int bookYear) {
+    title = bookTitle;
+    author = bookAuthor;
+    year = bookYear;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+  public String getAuthor() {
+    return author;
+  }
+  public int getYear() {
+    return year;
+  }
 }
