39025f9b53ad7b0cd7cbc5a0cabfdcfb410c4b78
Albert Salim <albertlimca@gmail.com>
2016-04-05 17:20:54 +0800

    newly created book should be available for loan

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -15,4 +15,10 @@ public class BookTest {
     assertEquals(harryPotter.getAuthor(), "J. K. Rowling");
     assertEquals(harryPotter.getYear(), 1997);
   }
+
+  @Test
+  public void newBookIsAvailableForLoan() {
+    Book harryPotter = new Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 1997);
+    assertTrue(harryPotter.isAvailable());
+  }
 }
