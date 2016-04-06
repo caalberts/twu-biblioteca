2f7e6e2a36b19d42cab66f3803661b9e58e36106
Albert Salim <albertlimca@gmail.com>
2016-04-06 15:09:18 +0800

    initialise library in Biblioteca

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 public class BibliotecaApp {
   private String userCommand = "";
 
-  Helper helper = new Helper();
-  Library library = new Library();
+  private Helper helper = new Helper();
+  private Library library = new Library();
 
   public void start() {
+    initializeLibrary();
+
     System.out.println("Welcome to Biblioteca.\n");
 
     userCommand = askUserForCommand();
@@ -48,7 +50,7 @@ public class BibliotecaApp {
     return;
   }
 
-  public String askUserForCommand() {
+  private String askUserForCommand() {
     String prompt = "How can I help you?\n" +
                     "Type any of the following commands:\n" +
                     "1. 'List Books'\n" +
@@ -56,5 +58,14 @@ public class BibliotecaApp {
                     "3. 'Return'\n" +
                     "4. 'Quit'\n";
     return helper.getUserInput(prompt);
+  }
+
+  private void initializeLibrary() {
+    library.addNewBook(new Book("Head First Java", "Kathy Sierra", 2005));
+    library.addNewBook(new Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 1997));
+    library.addNewBook(new Book("Test Driven Development", "Kent Beck", 2002));
+    library.addNewBook(new Book("A Game of Thrones", "George R. R. Martin", 1996));
+    library.addNewBook(new Book("Blink: The Power of Thinking Without Thinking", "Malcolm Gladwell", 2005));
+    library.addNewBook(new Book("The 5 Love Languages", "Gary Chapman", 1995));
   }
 }
